d80a83705ddcf1f4a1a8f0eba4d1316c0eae7ac2
Param Siddharth <paramsiddhartha@gmail.com>
2020-07-20 15:11:18 +0530

    'Pong3'

--- main.lua
+++ main.lua
@@ -4,13 +4,22 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 push = require 'push'
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
     smallFont = love.graphics.newFont('font.ttf', 8)
-    love.graphics.setFont(smallFont)
+
+    scoreFont = love.graphics.newFont('font.ttf', 32)
+
+    player1score = 0
+    player2score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 40
 
     push:setupScreen(
         VIRTUAL_WIDTH,
@@ -25,6 +34,20 @@ function love.load()
     )
 end
 
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    if love.keyboard.isDown('up') then
+        player2Y = player2Y - PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
+
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
@@ -36,11 +59,12 @@ function love.draw()
 
     love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
 
+    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
+
     love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
 
-    love.graphics.rectangle('fill', 5, 20, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 40, 5, 20)
-
+    love.graphics.setFont(smallFont)
     love.graphics.printf(
         "Hello Pong!",
         0, 
@@ -48,6 +72,10 @@ function love.draw()
         VIRTUAL_WIDTH, 
         'center'
     )
+
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(player1score, VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(player2score, VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 
     push:apply('end')
 end
