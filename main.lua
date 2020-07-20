5fd785332705cb403e81ed6bc422c23afd839a56
Param Siddharth <paramsiddhartha@gmail.com>
2020-07-20 14:49:55 +0530

    Pong2

--- main.lua
+++ main.lua
@@ -8,7 +8,10 @@ push = require 'push'
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
-    
+
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(
         VIRTUAL_WIDTH,
         VIRTUAL_HEIGHT,
@@ -22,18 +25,6 @@ function love.load()
     )
 end
 
---[[
-
-function love.draw()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-        fullscreen = false,
-        vsync = true,
-        resizable = false
-    })
-end
-
---]]
-
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
@@ -43,10 +34,17 @@ end
 function love.draw()
     push:apply('start')
 
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 5, 5)
+
+    love.graphics.rectangle('fill', 5, 20, 5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 40, 5, 20)
+
     love.graphics.printf(
         "Hello Pong!",
         0, 
-        VIRTUAL_HEIGHT / 2 - 6, 
+        20, 
         VIRTUAL_WIDTH, 
         'center'
     )
